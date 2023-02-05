8a873e629fa22dbfd2326123c1ad20f0ad6c2daf
Flojo-der-erste <fnettersheim@web.de>
2023-02-05 21:30:02 +0100

    Delete Test.py

--- source_code/KattiServices/Test.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import connect
-
-from source_code.KattiServices.MongoengineClasses.TestServiceDB import TestServiceDB
-
-if __name__ == '__main__':
-    connect()
-    test_service = TestServiceDB()
-    test_service.save()
